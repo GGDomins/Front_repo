7d1223eb7832f63dd6ed720e471f440a3b76e1dd
sway3 <svvoo31o@gmail.com>
2023-04-05 14:33:45 +0900

    login.js 수정

--- js/login.js
+++ js/login.js
@@ -1,14 +1,18 @@
+const id = document.getElementById('id');
+const password = document.getElementById('password');
+
 const form = document.getElementById('form');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    const dump = new FormData(form);
-    
-    fetch('http://127.0.0.1:5500', {
-        method: 'GET',
-        body: dump,
-    })
-    .then(res => res.json())
-    .then(data => console.log(data));
-});
+//     fetch('apikey', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             id: id,
+//             password: password,
+//         })
+//     })
+//         .then(response => response.json())
+//         .then(res => console.log(res));
+// });
